39f1747b42f29b22238eedf66a99474b3b21c3bd
muskaan Shekhawat <suman1shekhawat23@gmail.com>
2026-08-29 14:10:57 +0530

    Add password generator using secrets and string modules

---
+++ password_generator.py
@@ -0,0 +1,10 @@
+import secrets
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = ''.join(secrets.choice(characters) for _ in range(length))
+
+print("Generated Password:", password)
